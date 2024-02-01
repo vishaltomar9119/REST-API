755772c9c3e5821e07efb161714de8d6d852e337
vishaltomar9119 <110164948+vishaltomar9119@users.noreply.github.com>
2024-02-01 16:07:47 +0530

    Update models.js

--- model/models.js
+++ model/models.js
@@ -1,1 +1,56 @@
+module.exports = function (nosql){
+
+nosql.model('Hospital' , new nosql.Schema({
+    name: {
+        type: String,
+        required: true,
+    }
+}))
+
+nosql.model('Psychiatrist', new nosql.Schema({
+    name: {
+            type: String,
+            required: true,
+          },
+          hospitalId: {
+              type: String ,
+              required:true
+            },
+}));
+
+nosql.model('Patient', new nosql.Schema({
+         name: {
+            type: String,
+            required: true,
+          },
+          psychiatristId: {
+            type: String, 
+            default:null,
+          },
+          address: {
+            type: String,
+            default:null,
+          },
+          phone: {
+            type: Number,
+            required: true,
+          },
+          email: {
+            type: String,
+          },
+          password: {
+            type: String,
+            required: true,
+          },
+          photo: {
+            type: String,
+            default: null
+          },
+}));
+
+
+};
+
+
+
 
